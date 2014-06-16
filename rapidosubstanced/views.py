d58da0688e3a818a2a0fcbd79b694dc396898874
ebrehault <ebrehault@gmail.com>
2014-06-16 16:16:34 +0200

    import design

--- rapidosubstanced/views.py
+++ rapidosubstanced/views.py
@@ -1,9 +1,15 @@
+import json
+
 from pyramid.httpexceptions import HTTPFound
 from pyramid.view import view_config
+
+import colander
+import deform.widget
 
 from substanced.sdi import mgmt_view
 from substanced.form import FormView
 from substanced.interfaces import IFolder
+from substanced.schema import Schema
 
 from rapido.core.interfaces import IDatabase, IImporter
 
@@ -34,13 +40,36 @@ class AddDatabaseView(FormView):
         db.initialize()
         db.storage.soup
         
-        # TEST
-        importer = IImporter(db)
-        importer.import_database({'forms': {'frmBook': {'frmBook.py': "\ndef forever(context):\n    return 'I will never change.'", 'frmBook.yaml': 'assigned_rules: [polite]\nfields:\n  author: {index_type: text, type: TEXT}\n  famous_quote: {mode: COMPUTED_ON_SAVE, type: TEXT}\n  forever: {mode: COMPUTED_ON_CREATION, type: TEXT}\nid: frmBook\ntitle: Book form\n', 'frmBook.html': 'Author: <span data-rapido-field="author">author</span>'}}, 'settings.yaml': 'acl:\n  rights:\n    author: [FamousDiscoverers]\n    editor: []\n    manager: [admin]\n    reader: []\n  roles: {}\n'})
-
         return HTTPFound(
             self.request.sdiapi.mgmt_path(self.context, '@@contents')
             )
+
+class ImportSchema(Schema):
+    json_design = colander.SchemaNode(
+        colander.String(),
+        widget = deform.widget.TextAreaWidget(rows=10, cols=120),
+        title='Design as JSON',
+        )
+
+@mgmt_view(
+    content_type='Database',
+    name="import_export",
+    tab_title="Import/export",
+    permission='sdi.add-content',
+    renderer='templates/importexport.pt',
+    )
+class ImportExportView(FormView):
+    schema = ImportSchema(title='Import')
+    buttons = ('import',)
+
+    def import_success(self, data):
+        db = IDatabase(self.context)
+        importer = IImporter(db)
+        importer.import_database(json.loads(data['json_design']))
+        return HTTPFound(
+            location=self.request.sdiapi.mgmt_path(self.context, '@@contents')
+            )
+
 
 @mgmt_view(
     content_type='Database',
